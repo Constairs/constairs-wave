e90f61c0da9599fd9d1f009f3731eb8761196964
constairs <constairs@gmail.com>
2018-02-02 15:57:01 +0700

    Fixed styles & delet useless pages

--- app/js/common.js
+++ app/js/common.js
@@ -50,61 +50,106 @@ $(function() {
 		return false;
 	}); */
 
-	$("form").submit(function(){
-		var dat, fm, valid_name, valid_email;
-		fm = $(this);
-		dat = fm.serialize();
-		valid_name = fm.find('.validname');
-		valid_email = fm.find('.validemail');
-		valid_message = fm.find('.validmessage');
-		valid_name.css("border-color","#ccc");
-		valid_email.css("border-color","#ccc");
-		valid_message.css("border-color","#ccc");
+	// $("form").submit(function(){
+	// 	var dat, fm, valid_name, valid_email;
+	// 	fm = $(this);
+	// 	dat = fm.serialize();
+	// 	valid_name = fm.find('.validname');
+	// 	valid_email = fm.find('.validemail');
+	// 	valid_message = fm.find('.validmessage');
+	// 	valid_name.css("border-color","#ccc");
+	// 	valid_email.css("border-color","#ccc");
+	// 	valid_message.css("border-color","#ccc");
 
-		valid_name.blur(function() {
-			if(valid_name.val().length>1) {
-				$(this).css("border-color","rgba(86,155,68,.8)");
-			} else {
-				$(this).css("border-color","rgba(243,44,45,.8)");
-			}
-		});
+	// 	valid_name.blur(function() {
+	// 		if(valid_name.val().length>1) {
+	// 			$(this).css("border-color","rgba(86,155,68,.8)");
+	// 		} else {
+	// 			$(this).css("border-color","rgba(243,44,45,.8)");
+	// 		}
+	// 	});
 
-		valid_email.blur(function() {
-			if($(this).val() != '') {
-				var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
-				if(pattern.test($(this).val())){
-					$(this).css("border-color","rgba(86,155,68,.8)");
-					valid = 1;
-				} else {
-					$(this).css("border-color","rgba(243,44,45,.8)");
+	// 	valid_email.blur(function() {
+	// 		if($(this).val() != '') {
+	// 			var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+	// 			if(pattern.test($(this).val())){
+	// 				$(this).css("border-color","rgba(86,155,68,.8)");
+	// 				valid = 1;
+	// 			} else {
+	// 				$(this).css("border-color","rgba(243,44,45,.8)");
+	// 			}
+	// 		} else {
+	// 			$(this).css("border-color","rgba(243,44,45,.8)");
+	// 		}
+	// 	});
+
+	// 	if(valid_name.val().length>1 && valid_email.val().length>3){
+	// 		$.post("mail.php", {dat:dat},function(data) {
+	// 			$(".modal-complete, .ten").fadeIn(250);
+	// 			setTimeout(function() {
+	// 				fm.trigger("reset");
+	// 			}, 1000);
+	// 		});
+	// 		return false;
+	// 	}else{
+	// 		if(valid_name.val().length<1){
+	// 			valid_name.css("border-color","rgba(243,44,45,.8)");
+	// 		}            
+	// 		if(valid_email.val().length<3){
+	// 			valid_email.css("border-color","rgba(243,44,45,.8)");
+	// 		}
+	// 		return false; 
+	// 	}
+	// }); // end validate
+
+
+	$("#form").validate({
+		submitHandler: function(form) {
+
+			form.submit(function() {
+				var th = $(this);
+				$.ajax({
+					type: "POST",
+					url: "mail.php",
+					data: th.serialize()
+				}).done(function() {
+					$(".modal-complete, .ten").fadeIn(250);
+					setTimeout(function() {
+						th.trigger("reset");
+					}, 1000);
+				});
+				return false;
+			}); 
+
+		},
+		rules: {
+			message: {
+				required: false
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			name: {
+				required: true,
+				minlength: 3
+			},
+			messages: {
+				email: {
+					required: 'Поле обязательно для заполнения'
+				},
+				name: {
+					required: 'Имя должно быть обязательно заполнено',
+					minlength: 'Длина имени должна быть не менее 3-х символов'
+					// jQuery.format('Длина имени должна быть не менее {0}-х символов')
 				}
-			} else {
-				$(this).css("border-color","rgba(243,44,45,.8)");
 			}
-		});
-
-		if(valid_name.val().length>1 && valid_email.val().length>3){
-			$.post("mail.php", {dat:dat},function(data) {
-				$(".modal-complete, .ten").fadeIn(250);
-				setTimeout(function() {
-					fm.trigger("reset");
-				}, 1000);
-			});
-			return false;
-		}else{
-			if(valid_name.val().length<1){
-				valid_name.css("border-color","rgba(243,44,45,.8)");
-			}            
-			if(valid_email.val().length<3){
-				valid_email.css("border-color","rgba(243,44,45,.8)");
-			}
-			return false; 
 		}
-	}); // end validate
+	});
 
 });// end ready
 
 $(window).load(function() {
-	$(".loader_inner").fadeOut(); 
-	$(".loader").delay(400).fadeOut("slow"); 
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
 });
